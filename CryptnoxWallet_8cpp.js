9fc97af1fb20809ee0bba88da75134cfc3aacf82
embarquech <embarquech@users.noreply.github.com>
2025-12-18 11:03:54 +0000

    deploy: 690449e85cd1d2becb06ec6cdbf2f758f7bfe9d8

--- CryptnoxWallet_8cpp.js
+++ CryptnoxWallet_8cpp.js
@@ -11,6 +11,7 @@ var CryptnoxWallet_8cpp =
     [ "OPENSECURECHANNEL_SALT_IN_BYTES", "CryptnoxWallet_8cpp.html#a5844cc731e45bc22143e8b5df0e5bbbe", null ],
     [ "RANDOM_BYTES", "CryptnoxWallet_8cpp.html#af1f0cb989a33a574c24035af703f414a", null ],
     [ "RESPONSE_GETCARDCERTIFICATE_IN_BYTES", "CryptnoxWallet_8cpp.html#a3c25c23f5fe90be1ba5057da3671e497", null ],
+    [ "RESPONSE_OPENSECURECHANNEL_2_IN_BYTES", "CryptnoxWallet_8cpp.html#ab51256e3dacd1b73f07d566bd534d779", null ],
     [ "RESPONSE_OPENSECURECHANNEL_IN_BYTES", "CryptnoxWallet_8cpp.html#a7c5aeb3574b13dac470b43046615b00a", null ],
     [ "RESPONSE_SELECT_IN_BYTES", "CryptnoxWallet_8cpp.html#a460ee1a0f1a0e3420b7f4f621b35157b", null ],
     [ "RESPONSE_STATUS_WORDS_IN_BYTES", "CryptnoxWallet_8cpp.html#a06c6580716e31d8e2dc06ce292908f0c", null ]
--- search/all_9.js
+++ search/all_9.js
@@ -4,7 +4,8 @@ var searchData=
   ['readuid_1',['readuid',['../classCryptnoxWallet.html#a2e90b4ce65336ba84d371deab5144274',1,'CryptnoxWallet::readUID()'],['../classPN532Base.html#a72201197b549a93016234387f255cf7b',1,'PN532Base::readUID(uint8_t *uidBuffer, uint8_t &amp;uidLength)']]],
   ['resetreader_2',['resetReader',['../classPN532Base.html#a2da00ab197b3c3f665f124c2f41db730',1,'PN532Base']]],
   ['response_5fgetcardcertificate_5fin_5fbytes_3',['RESPONSE_GETCARDCERTIFICATE_IN_BYTES',['../CryptnoxWallet_8cpp.html#a3c25c23f5fe90be1ba5057da3671e497',1,'CryptnoxWallet.cpp']]],
-  ['response_5fopensecurechannel_5fin_5fbytes_4',['RESPONSE_OPENSECURECHANNEL_IN_BYTES',['../CryptnoxWallet_8cpp.html#a7c5aeb3574b13dac470b43046615b00a',1,'CryptnoxWallet.cpp']]],
-  ['response_5fselect_5fin_5fbytes_5',['RESPONSE_SELECT_IN_BYTES',['../CryptnoxWallet_8cpp.html#a460ee1a0f1a0e3420b7f4f621b35157b',1,'CryptnoxWallet.cpp']]],
-  ['response_5fstatus_5fwords_5fin_5fbytes_6',['RESPONSE_STATUS_WORDS_IN_BYTES',['../CryptnoxWallet_8cpp.html#a06c6580716e31d8e2dc06ce292908f0c',1,'CryptnoxWallet.cpp']]]
+  ['response_5fopensecurechannel_5f2_5fin_5fbytes_4',['RESPONSE_OPENSECURECHANNEL_2_IN_BYTES',['../CryptnoxWallet_8cpp.html#ab51256e3dacd1b73f07d566bd534d779',1,'CryptnoxWallet.cpp']]],
+  ['response_5fopensecurechannel_5fin_5fbytes_5',['RESPONSE_OPENSECURECHANNEL_IN_BYTES',['../CryptnoxWallet_8cpp.html#a7c5aeb3574b13dac470b43046615b00a',1,'CryptnoxWallet.cpp']]],
+  ['response_5fselect_5fin_5fbytes_6',['RESPONSE_SELECT_IN_BYTES',['../CryptnoxWallet_8cpp.html#a460ee1a0f1a0e3420b7f4f621b35157b',1,'CryptnoxWallet.cpp']]],
+  ['response_5fstatus_5fwords_5fin_5fbytes_7',['RESPONSE_STATUS_WORDS_IN_BYTES',['../CryptnoxWallet_8cpp.html#a06c6580716e31d8e2dc06ce292908f0c',1,'CryptnoxWallet.cpp']]]
 ];
