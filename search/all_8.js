e56bc78e4dcd4f1208f05a4ccfc2d807170e5bd2
embarquech <embarquech@users.noreply.github.com>
2025-12-24 20:02:55 +0000

    deploy: db7e678eef78d93f70f9b10dd8797bcaf115e206

--- search/all_8.js
+++ search/all_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['opensecurechannel_0',['openSecureChannel',['../classCryptnoxWallet.html#a5e31e6d45992e39db06b8a05ca70bddd',1,'CryptnoxWallet']]],
-  ['opensecurechannel_5fsalt_5fin_5fbytes_1',['OPENSECURECHANNEL_SALT_IN_BYTES',['../CryptnoxWallet_8cpp.html#a5844cc731e45bc22143e8b5df0e5bbbe',1,'CryptnoxWallet.cpp']]]
+  ['opensecurechannel_5fsalt_5fin_5fbytes_1',['OPENSECURECHANNEL_SALT_IN_BYTES',['../CryptnoxWallet_8cpp.html#a5844cc731e45bc22143e8b5df0e5bbbe',1,'CryptnoxWallet.cpp']]],
+  ['operator_3d_2',['operator=',['../classPN532Adapter.html#a5d7a62eca0c1763f5f1b7472be5a4741',1,'PN532Adapter']]]
 ];
--- search/functions_7.js
+++ search/functions_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['opensecurechannel_0',['openSecureChannel',['../classCryptnoxWallet.html#a5e31e6d45992e39db06b8a05ca70bddd',1,'CryptnoxWallet']]]
+  ['opensecurechannel_0',['openSecureChannel',['../classCryptnoxWallet.html#a5e31e6d45992e39db06b8a05ca70bddd',1,'CryptnoxWallet']]],
+  ['operator_3d_1',['operator=',['../classPN532Adapter.html#a5d7a62eca0c1763f5f1b7472be5a4741',1,'PN532Adapter']]]
 ];
