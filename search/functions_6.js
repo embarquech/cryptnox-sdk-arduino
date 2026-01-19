9f1690d1f4a0a09f94fe0fa8740a83fa7bf65c8d
YanNaingWinn <YanNaingWinn@users.noreply.github.com>
2026-01-19 14:19:21 +0000

    deploy: cba4d29c630582ab8b7fda882ae77ea2d32449eb

--- search/functions_6.js
+++ search/functions_6.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['inlistpassivetarget_0',['inlistpassivetarget',['../classNFCDriver.html#a9370430c2fffb8288a55f78c1ff5dcfd',1,'NFCDriver::inListPassiveTarget()'],['../classPN532Adapter.html#ac28bae87cad3c9c0d150d2cb4c04259b',1,'PN532Adapter::inListPassiveTarget()']]],
-  ['issecurechannelopen_1',['isSecureChannelOpen',['../classCryptnoxWallet.html#a08c6fa72f255be4cff6113187f8e3218',1,'CryptnoxWallet']]]
+  ['inlistpassivetarget_0',['inlistpassivetarget',['../classNFCDriver.html#a9370430c2fffb8288a55f78c1ff5dcfd',1,'NFCDriver::inListPassiveTarget()'],['../classPN532Adapter.html#ac28bae87cad3c9c0d150d2cb4c04259b',1,'PN532Adapter::inListPassiveTarget()']]]
 ];
