94d485f73a7366e72c0ff22b494782d73606a8bb
YanNaingWinn <YanNaingWinn@users.noreply.github.com>
2026-01-19 14:20:29 +0000

    deploy: 2c17e42597845dbcf1620d587801d24b7cdaa000

--- classCryptnoxWallet.js
+++ classCryptnoxWallet.js
@@ -5,6 +5,7 @@ var classCryptnoxWallet =
     [ "detectCard", "classCryptnoxWallet.html#a2be0791a38aa70aaa1ee06f75e0f1704", null ],
     [ "establishSecureChannel", "classCryptnoxWallet.html#a8584e36b3c8c088ad1ee18358246262e", null ],
     [ "getCardInfo", "classCryptnoxWallet.html#a14a8f392e44c6960205aa6514f99b34f", null ],
+    [ "isSecureChannelOpen", "classCryptnoxWallet.html#a08c6fa72f255be4cff6113187f8e3218", null ],
     [ "processCard", "classCryptnoxWallet.html#aa4ce170f582927c6e8e8d4e9dc4d412d", null ],
     [ "resetReader", "classCryptnoxWallet.html#a1ff890db8c53c5dd07e5dd30856b6a36", null ],
     [ "verifyPin", "classCryptnoxWallet.html#af2a97d9d9db0adefe49f09488d8b3d31", null ]
